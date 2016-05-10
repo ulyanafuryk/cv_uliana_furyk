0d335a2128b6e5ef2b199fb67611c27539cf6cc2
ulyana <uljka@yahoo.com>
2016-05-10 07:11:42 +0000

    fvty

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
-$(function() {
-    $('#btn-send').click(function() {
-        $.ajax({
-            url: "https://formspree.io/uljka@yahoo.com", 
-            method: "POST",
-            data: {
-                name: name.value,
-                email: email.value,
-                message: message.value},
-            dataType: "json"
-        })
-        .done(function () {
-            $('#forma').html('<h1>Thank you!</h1>');
-        })
-        .fail(function (jqXHR, textStatus) {
-            $('#forma').html('<h1>Error!</h1><br>'+textStatus);
-        });
-    })
+$(function () {
+	'use strict';
+	console.log("ready!");
+
+	$('#contact').submit(function (event) {
+		$.ajax({
+			url: "https://formspree.io/uljka@yahoo.com",
+			method: "POST",
+			data: {
+				name: firstname.value,
+				email: email.value,
+				message: msg.value
+			},
+			dataType: "json"
+		}).done(function () {
+			$("#firstname").val("");
+			$("#email").val("");
+			$("#msg").val("");
+			$('#form').html('<h1>Thank you!</h1>');
+		});
+		event.preventDefault();
+	});
+});
+
+
+function scrollToId(whereToScroll) {
+    $('html, body').animate({
+        scrollTop: $(whereToScroll).offset().top
+    }, 800);
+    return true;
+}
+
+$(function () {
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
 })
+
+$(function() {
+	$(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+});
